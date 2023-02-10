41575d60c6f247e10de33a99d6166e9ec75d9a13
Jordan LaPrise <officialtoxicdub@gmail.com>
2023-02-09 15:10:59 -0900

    add filter options to callback

--- src/registry/event_registry.rs
+++ src/registry/event_registry.rs
@@ -5,12 +5,17 @@ use std::{
 
 type EventCallback<'a, T> = Box<dyn FnMut(&T) + Send + Sync + 'a>;
 
+struct EventSubscriptionArgs<'a, F>(Box<dyn FnMut(&dyn Any) + Send + Sync + 'a>, Option<F>);
+
 #[derive(Default)]
-struct EventRegistry<'a> {
-  registry: HashMap<TypeId, Vec<Box<dyn FnMut(&dyn Any) + Send + Sync + 'a>>>,
+pub struct EventRegistry<'a, F> {
+  registry: HashMap<TypeId, Vec<EventSubscriptionArgs<'a, F>>>,
 }
 
-impl<'a> EventRegistry<'a> {
+impl<'a, F> EventRegistry<'a, F>
+where
+  F: Default,
+{
   pub fn new() -> Self {
     Self {
       ..Default::default()
@@ -18,6 +23,13 @@ impl<'a> EventRegistry<'a> {
   }
 
   pub fn subscribe<T>(&mut self, callback: impl FnMut(&T) + Send + Sync + 'a)
+  where
+    T: 'a + Any + Send + Sync,
+  {
+    self.subscribe_with_filter(callback, None)
+  }
+
+  pub fn subscribe_with_filter<T>(&mut self, callback: impl FnMut(&T) + Send + Sync + 'a, filter: Option<F>)
   where
     T: 'a + Any + Send + Sync,
   {
@@ -31,7 +43,7 @@ impl<'a> EventRegistry<'a> {
     }) as Box<dyn FnMut(&dyn Any) + Send + Sync + 'a>;
 
     let callbacks = self.registry.entry(type_id).or_insert(Vec::new());
-    callbacks.push(callback);
+    callbacks.push(EventSubscriptionArgs(callback, filter));
   }
 
   pub fn invoke<T>(&mut self, event: &T)
@@ -42,9 +54,18 @@ impl<'a> EventRegistry<'a> {
 
     if let Some(callbacks) = self.registry.get_mut(&type_id) {
       for callback in callbacks {
-        callback(event);
+        callback.0(event);
       }
     }
+  }
+
+  pub fn get_subscriptions<T> (&self) -> Option<&Vec<EventSubscriptionArgs<'a, F>>>
+  where
+    T: 'a + Any + Send + Sync,
+  {
+    let type_id = TypeId::of::<T>();
+
+    self.registry.get(&type_id)
   }
 }
 
@@ -52,13 +73,15 @@ impl<'a> EventRegistry<'a> {
 mod event_registry_tests {
   use std::sync::{Arc, Mutex};
 
-  use super::*;
+  use crate::filter;
+
+use super::*;
 
   struct MyEvent {}
 
   #[test]
   fn test_event_registry() {
-    let mut registry = EventRegistry::new();
+    let mut registry: EventRegistry<f32> = EventRegistry::new();
 
     let counter: Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
     let c_ref = counter.clone();
@@ -72,5 +95,17 @@ mod event_registry_tests {
     registry.invoke(&MyEvent {});
 
     assert_eq!(2, *counter.lock().unwrap());
+  }
+
+  fn test_filter_registry() {
+    let mut registry:EventRegistry<Vec<TypeId>> = EventRegistry::new();
+
+    registry.subscribe_with_filter::<i32> (|_|{}, Some(filter![i32, i64]));
+    let subscriptions = registry.get_subscriptions::<i32>().unwrap();
+    assert_eq!(subscriptions.len(), 1);
+
+    let filter = subscriptions[0].1.as_ref().unwrap();
+    assert_eq!(filter.len(), 2);
+    assert_eq!(filter, &filter![i32, i64])
   }
 }
